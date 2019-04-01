9c474f754d9556463b6efa9709403262422059e5
Steven Derycke <stevenderycke244@gmail.com>
2019-04-01 20:35:45 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,9 @@
+<html>
+ <head>
+ </head>
+ <body>
+ <?php echo 'App Version: 18.0. Pod IP: '; ?>
+ <?php echo $_SERVER['SERVER_ADDR']; ?>
+ <?php echo ' PHP Version: ' . phpversion(); ?>
+ </body>
+</html>
