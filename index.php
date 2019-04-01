666439f5ae7e77f9f2a7fc897ffbce6d242dcedc
Steven Derycke <43333546+stevenderycke@users.noreply.github.com>
2019-04-01 20:46:48 +0200

    v2

--- index.php
+++ index.php
@@ -2,8 +2,8 @@
  <head>
  </head>
  <body>
- <?php echo 'App Version: 0.1. Pod IP: '; ?>
- <?php echo $_SERVER['SERVER_ADDR']; ?>
- <?php echo ' PHP Version: ' . phpversion(); ?>
+  <?php echo 'App Version: 0.2. Pod IP: '; ?></br>
+ <?php echo $_SERVER['SERVER_ADDR']; ?></br>
+ <?php echo ' PHP Version: ' . phpversion(); ?></br>
  </body>
 </html>
